746a04afb895bf5e4b46beabe86bbc3e22d1c40c
mateus-itzhari <mateus.itzhari@gmail.com>
2021-11-27 04:33:33 -0400

    criação da página home.dart

---
+++ lib/View/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:core';
+
+class Home extends StatefulWidget
+{
+  const Home({ Key? key }) : super(key: key);
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home>
+{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Gerador de Loteria")),
+      body: const Center(child: Text("Gerador")),
+    );
+  }
+}
